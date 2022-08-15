5deecbfa6a73e11ba5bc7f0c4d957ff5df088e4e
aagammehta999 <aagammehta49@gmail.com>
2022-08-16 00:45:40 +0530

    chnage

--- shopholic/settings.py
+++ shopholic/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 
 from pathlib import Path
+import os
 from decouple import config 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=config('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','f547=!=-o13m+)m4x1k&%+*o(d-_9sxa-z+9p94e)_%5wl)6ic' )
+# SECRET_KEY=config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 
